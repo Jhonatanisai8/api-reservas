4845e842d2fedee68a4774eceb5d9129981896c6
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-11 23:31:28 -0500

    controlador de la clase huesped: implementacion de los metodos para listat todos los huespedes (GET-MAPPING) y para guardar un Huesped (POST-MAPPING)

---
+++ src/main/java/com/api/api_reservas/controllers/GuestController.java
@@ -0,0 +1,31 @@
+package com.api.api_reservas.controllers;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.api_reservas.models.Guest;
+import com.api.api_reservas.services.GuestService;
+
+@RestController
+@RequestMapping("api/v1/guests")
+public class GuestController {
+
+    @Autowired
+    private GuestService guestService;
+
+    // metodos
+    @GetMapping
+    public List<Guest> getAllGuests() {
+        return guestService.getAllGuests();
+    }
+
+    @PostMapping
+    public Guest saveGuest(Guest guest) {
+        return guestService.saveGuest(guest);
+    }
+
+}
