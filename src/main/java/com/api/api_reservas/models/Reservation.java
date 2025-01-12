4d13cc68be59cd891e16041166fb15660850d886
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-11 23:11:10 -0500

    mapeo de la entidad reserva

---
+++ src/main/java/com/api/api_reservas/models/Reservation.java
@@ -0,0 +1,21 @@
+package com.api.api_reservas.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idReservation;
+    private String dateEntry;
+    private String DateDeparture;
+    private Double value;
+    private String FormPayment;
+
+}
